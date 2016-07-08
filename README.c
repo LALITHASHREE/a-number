f94cc8e614c0cb29f1b361be1aeccd02500be87e
LALITHASHREE <dlalithashree@gmail.com>
2016-07-08 12:16:52 +0530

    finish

---
+++ README.c
@@ -0,0 +1,19 @@
+# a-number
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,n;
+clrscr();
+printf("enter the value of a");
+scanf("%d",&a);
+if(a=n^2)
+{
+printf("enter the value of a");
+}
+else 
+{
+printf(given number is not a square number");
+}
+return0;
+}
